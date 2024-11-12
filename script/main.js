a79dbe9a4682b10dff5963ba2d434d34e5ae4665
Estefania <emanjongonzalez@gmail.com>
2024-11-12 09:27:50 +0100

    version 0.3

--- script/main.js
+++ script/main.js
@@ -1,13 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let currentIndex = 0;
     const images = document.querySelectorAll(".carousel-images img");
+    const carouselContainer = document.querySelector(".carousel-images");
+    let currentIndex = 0;
     const totalImages = images.length;
 
     function showNextImage() {
         currentIndex = (currentIndex + 1) % totalImages;
-        const offset = -currentIndex * 100; // Mover el carrusel
-        document.querySelector(".carousel-images").style.transform = `translateX(${offset}%)`;
+        const offset = -currentIndex * 100; // Desplaza en unidades de 100%
+        carouselContainer.style.transform = `translateX(${offset}%)`;
     }
 
-    setInterval(showNextImage, 3000); // Cambia de imagen cada 3 segundos
+    setInterval(showNextImage, 6000); // Cambia de imagen cada 6 segundos
 });
